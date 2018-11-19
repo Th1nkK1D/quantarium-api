979e11b4f74170b5cff6eeaf78acad4f24969f85
Withee Poositasai <witheep@gmail.com>
2018-11-19 15:33:17 +0700

    Add swagger-ui-dist

--- app/index.js
+++ app/index.js
@@ -1,10 +1,17 @@
 const Koa = require('koa')
+const serve = require('koa-static')
+const mount = require('koa-mount')
+const pathToSwaggerUi = require('swagger-ui-dist').absolutePath()
+
+async function api(ctx, next){
+  await next();
+  ctx.body = 'Hello';
+}
 
 const app = new Koa()
 
-app.use(async ctx => {
-  ctx.body = 'Hello World'
-})
+app.use(mount('/', serve(pathToSwaggerUi)))
+app.use(mount('/api', api))
 
 app.listen(3000);
 

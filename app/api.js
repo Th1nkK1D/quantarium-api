ca2cb84683bfcc284c9751a92e9b30e7dd62935b
Withee Poositasai <witheep@gmail.com>
2018-11-25 21:59:58 +0700

    Integrate with qsim, main api function is working

--- app/api.js
+++ app/api.js
@@ -2,8 +2,19 @@ import Koa from 'koa'
 import Router from 'koa-router'
 import Logger from 'koa-logger'
 
+import { Qubit, BasicGate } from '../../quantarium-qsim/lib'
+
 const api = new Koa()
 const router = new Router()
+
+let q = new Qubit()
+
+function getErrorResBody(msg) {
+  return {
+    error: true,
+    msg: msg
+  }
+}
 
 /**
  * @swagger
@@ -12,16 +23,27 @@ const router = new Router()
  *    properties:
  *      isCollapsed:
  *        type: boolean
+ *      state:
+ *        type: array
+ *        items:
+*           $ref: '#/definitions/state'
  *      gates:
  *        $ref: '#/definitions/gates'
- *      measurement:
- *        $ref: '#/definitions/measurement'
  */
 
 /**
  * @swagger
  * definition:
  *  gates:
+ *    type: array
+ *    items:
+ *      type: string 
+ */
+
+/**
+ * @swagger
+ * definition:
+ *  state:
  *    type: array
  *    items:
  *      type: string 
@@ -42,27 +64,10 @@ const router = new Router()
 
 router.get('/', async (ctx, next) => {
   await next()
-  ctx.body = 'Welcome to Quantarium Composer API'
-})
-
-/**
- * @swagger
- * /init:
- *   post:
- *     tags:
- *       - Common
- *     description: Initialize composer
- *     produces:
- *       - application/json
- *     responses:
- *       200:
- *         description: Composer state
- *         schema:
- *           $ref: '#/definitions/composer-state'
- */
-router.post('/init', async (ctx, next) => {
-  await next()
-  ctx.body = {}
+  ctx.status = 200
+  ctx.body = {
+    msg: 'Welcome to Quantarium Composer API'
+  }
 })
 
 /**
@@ -82,7 +87,8 @@ router.post('/init', async (ctx, next) => {
  */
 router.get('/state', async (ctx, next) => {
   await next()
-  ctx.body = {}
+  ctx.status = 200
+  ctx.body = q.getQubitSummary()
 })
 
 /**
@@ -102,7 +108,171 @@ router.get('/state', async (ctx, next) => {
  */
 router.post('/reset', async (ctx, next) => {
   await next()
-  ctx.body = {}
+  
+  ctx.status = 200
+  ctx.body = q.reset()
+})
+
+/**
+ * @swagger
+ * /gate/{gate-symbol}:
+ *   get:
+ *     tags:
+ *       - Gate Operation
+ *     description: Preview gate result
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Next qubit state
+ *         schema:
+ *           $ref: '#/definitions/state'
+ */
+router.get('/gate/:gateSymbol', async (ctx, next) => {
+  await next()
+
+  const gate = ctx.params.gateSymbol ? ctx.params.gateSymbol.toUpperCase() : false
+
+  if (!(gate in BasicGate)) {
+    ctx.status = 400
+    ctx.body = getErrorResBody('Gate not available')
+  } else if (q.collapsed !== false) {
+    ctx.status = 400
+    ctx.body = getErrorResBody('Qubit is collapsed')
+  } else {
+    ctx.status = 200
+    ctx.body = {
+      state: q.calculateOperation(BasicGate[gate].operation).map(z => z.toString())
+    }
+  }
+
+})
+
+/**
+ * @swagger
+ * /gate/{gate-symbol}:
+ *   put:
+ *     tags:
+ *       - Gate Operation
+ *     description: Apply gate to the qubit
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Composer state
+ *         schema:
+ *           $ref: '#/definitions/composer-state'
+ */
+router.put('/gate/:gateSymbol', async (ctx, next) => {
+  await next()
+
+  const gate = ctx.params.gateSymbol ? ctx.params.gateSymbol.toUpperCase() : false
+
+  if (!(gate in BasicGate)) {
+    ctx.status = 400
+    ctx.body = getErrorResBody('Gate is not available')
+  } else {
+    const opRes = q.pushGates([BasicGate[gate]])
+
+    if (!opRes) {
+      ctx.status = 400
+      ctx.body = getErrorResBody('Qubit is collapsed')
+    } else {
+      ctx.status = 200
+      ctx.body = opRes
+    }
+  }
+
+})
+
+/**
+ * @swagger
+ * /gate/{gate-symbol}:
+ *   delete:
+ *     tags:
+ *       - Gate Operation
+ *     description: Remove last applied gate from the qubit
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Composer state
+ *         schema:
+ *           $ref: '#/definitions/composer-state'
+ */
+router.delete('/gate/', async (ctx, next) => {
+  await next()
+
+  const opRes = q.popGates()
+
+  if (!opRes) {
+    ctx.status = 400
+    ctx.body = getErrorResBody('Qubit is collapsed')
+  } else {
+    ctx.status = 200
+    ctx.body = opRes
+  }
+
+})
+
+/**
+ * @swagger
+ * /measure/{batch-size}:
+ *   post:
+ *     tags:
+ *       - Measurement
+ *     description: Measure the qubit
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Measurement result
+ *         schema:
+ *           $ref: '#/definitions/measurement'
+ */
+router.post('/measure/:batchSize', async (ctx, next) => {
+  await next()
+
+  const opRes = q.measure(ctx.params.batchSize)
+
+  if (!opRes) {
+    ctx.status = 400
+    ctx.body = getErrorResBody('Qubit is collapsed')
+  } else {
+    ctx.status = 200
+    ctx.body = opRes
+  }
+
+})
+
+/**
+ * @swagger
+ * /unmeasure:
+ *   post:
+ *     tags:
+ *       - Measurement
+ *     description: Undo measurement
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Composer state
+ *         schema:
+ *           $ref: '#/definitions/composer-state'
+ */
+router.post('/unmeasure/', async (ctx, next) => {
+  await next()
+
+  const opRes = q.unmeasure()
+
+  if (!opRes) {
+    ctx.status = 400
+    ctx.body = getErrorResBody('No measurement was applied')
+  } else {
+    ctx.status = 200
+    ctx.body = opRes
+  }
+
 })
 
 api.use(Logger())
